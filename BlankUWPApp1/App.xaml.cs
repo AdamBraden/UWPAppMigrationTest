47d5e80b78c26240fa6cbf1f3da6eddd41410cb0
Adam Braden <adambr@microsoft.com>
2021-02-03 20:48:40 -0800

    Add project files.

---
+++ BlankUWPApp1/App.xaml.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Notifications;
+using System.Runtime.InteropServices;
+using Windows.Data.Xml.Dom;
+#if WINDOWS_UWP
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+#else
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using WinRT;
+#endif
+
+namespace BlankUWPApp1
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
+#if WINDOWS_UWP
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+#else
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs e)
+#endif
+        {
+
+#if WINDOWS_UWP
+            var curWindow = Window.Current;
+#else
+            m_window = new Window();
+            m_window.Content = new Frame();
+            var curWindow = m_window;
+            curWindow.Title = "My Test App - hWnd";
+            //Intialize interop helper for dialog parenting scenarios
+            var windowNative = m_window.As<IWindowNative>();
+            m_windowHandle = windowNative.WindowHandle;
+
+            // Initialize the main Page to load into Window
+            m_page = new MainPage();
+
+            //Hook up On*Activated events
+            //Could be here or Main
+            //Most likely at end of the method
+            var activationArgs = AppInstance.GetActivatedEventArgs();
+            if (activationArgs.Kind == ActivationKind.File)
+            {
+                OnFileActivated(activationArgs as FileActivatedEventArgs);
+            }
+#endif
+
+            Frame rootFrame = curWindow.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+#if WINDOWS_UWP
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+#endif
+                // Place the frame in the current Window
+                curWindow.Content = rootFrame;
+            }
+
+
+#if WINDOWS_UWP
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    // When the navigation stack isn't restored navigate to the first page,
+                    // configuring the new page by passing required information as a navigation
+                    // parameter
+
+                }      
+                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+            }
+#else
+            //Q: How does UWP know what the 'first' page is?
+            rootFrame.Navigate(typeof(MainPage), m_page);
+
+#endif
+
+            // Ensure the current window is active
+            curWindow.Activate();
+
+        }
+
+        protected override void OnFileActivated(FileActivatedEventArgs args)
+        {
+            base.OnFileActivated(args);
+
+            // TODO: Handle file activation
+            // The number of files received is args.Files.Size
+            // The name of the first file is args.Files[0].Name
+
+            XmlDocument toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText02);
+            XmlNodeList textElements = toastXml.GetElementsByTagName("text");
+            textElements[0].AppendChild(toastXml.CreateTextNode("My Test Application"));
+            textElements[1].AppendChild(toastXml.CreateTextNode($"File Type activated: {args.Files[0].Name}"));
+            ToastNotification notification = new ToastNotification(toastXml);
+            notification.ExpirationTime = DateTime.Now.AddMinutes(5);
+            ToastNotificationManager.CreateToastNotifier().Show(notification);
+        }
+
+
+
+        /// <summary>
+        /// Invoked when Navigation to a certain page fails
+        /// </summary>
+        /// <param name="sender">The Frame which failed navigation</param>
+        /// <param name="e">Details about the navigation failure</param>
+#if WINDOWS_UWP
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+#else
+        void OnNavigationFailed(object sender, Microsoft.UI.Xaml.Navigation.NavigationFailedEventArgs e)
+#endif
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+
+        private Window m_window;
+        private Page m_page;
+        private IntPtr m_windowHandle;
+        public IntPtr WindowHandle { get { return m_windowHandle; } }
+
+        [ComImport]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        [Guid("EECDBF0E-BAE9-4CB6-A68E-9598E1CB57BB")]
+        internal interface IWindowNative
+        {
+            IntPtr WindowHandle { get; }
+        }
+    }
+}
---
+++ BlankUWPApp1/MainPage.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Search;
+using Windows.UI.Notifications;
+using Windows.UI.Popups;
+using System.Runtime.InteropServices;
+#if WINDOWS_UWP
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+#else
+using WinRT;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+//using Windows.UI.Xaml.Controls.Primitives;
+//using Windows.UI.Xaml.Data;
+//using Windows.UI.Xaml.Input;
+//using Windows.UI.Xaml.Media;
+//using Windows.UI.Xaml.Navigation;
+#endif
+using Windows.Data.Xml.Dom;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BlankUWPApp1
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+            this.SizeChanged += MainPage_SizeChanged;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var showDialog = new MessageDialog("Read nuget packages folder?");
+            var folderPicker = new Windows.Storage.Pickers.FolderPicker();
+
+#if !WINDOWS_UWP
+            //Interop helpers for non-CoreWindow
+            ParentDialogToWindow(showDialog);
+            ParentDialogToWindow(folderPicker);
+#endif
+
+            //show a prompt
+            await showDialog.ShowAsync();
+
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+
+            folderPicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.ComputerFolder;
+            folderPicker.FileTypeFilter.Add("*");
+
+            StorageFolder folder = await folderPicker.PickSingleFolderAsync();
+            if (folder != null)
+            {
+                // Application now has read/write access to all contents in the picked folder
+                // (including other sub-folder contents)
+                Windows.Storage.AccessCache.StorageApplicationPermissions.FutureAccessList.AddOrReplace("PickedFolderToken", folder);
+
+                // Get the first folder in the current folder, sorted by date.
+                IReadOnlyList<StorageFolder> sortedItems = await folder.GetFoldersAsync();
+                var items = sortedItems.Select(x => x.Name).ToList();
+                dirList.Text = string.Join("\n", items);
+
+            }
+            //fails under app container
+            //dirList.Text s = string.Join("\n",System.IO.Directory.GetDirectories(path));
+
+            }
+
+        /// If the page is resized to less than 500 pixels, use the layout for narrow widths. 
+        /// If the page is resized so that the width is less than the height, use the tall (portrait) layout.
+        /// Otherwise, use the default layout.
+        void MainPage_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            if (e.NewSize.Width < 500)
+            {
+                VisualStateManager.GoToState(this, "MinimalLayout", true);
+            }
+            else if (e.NewSize.Width < e.NewSize.Height)
+            {
+                VisualStateManager.GoToState(this, "PortraitLayout", true);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, "DefaultLayout", true);
+            }
+        }
+        public void PrintStars(string s)
+        {
+            //C# 7 feature
+            if (int.TryParse(s, out var i)) { System.Diagnostics.Debug.WriteLine(new string('*', i)); }
+            else { Debug.WriteLine("Cloudy - no stars tonight!"); }
+        }
+
+#if !WINDOWS_UWP
+        private static void ParentDialogToWindow(object dialog)
+        {
+            //Interop helpers for non-CoreWindow
+            IntPtr windowHandle = (App.Current as App).WindowHandle;
+            var initializeWithWindow = dialog.As<IInitializeWithWindow>();
+            initializeWithWindow.Initialize(windowHandle);
+        }
+#endif
+
+        //CoreWindow interop helper, pass in your hwnd
+        [ComImport]
+        [Guid("3E68D4BD-7135-4D10-8018-9FB6D9F33FA1")]
+        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        public interface IInitializeWithWindow
+        {
+            void Initialize(IntPtr hwnd);
+        }
+
+    }
+}
